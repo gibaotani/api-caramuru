2cb998fa7a4ac962fef1b3e9d32aff2860022178
Peddnator <pejanini@gmail.com>
2019-11-18 15:04:04 -0300

    Restruturação do servlet HammerServlet para adicionar Membros   - Atualização dos metodos de membroDao para comportar inserção no     Cloud Firestore

--- java/src/main/java/HammerServlet.java
+++ java/src/main/java/HammerServlet.java
@@ -1,72 +1,47 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
-import java.util.Map;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.SimpleTimeZone;
 import java.util.concurrent.ExecutionException;
-
-import javax.print.attribute.standard.DocumentName;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.crypto.Data;
-
-import com.google.api.core.ApiFuture;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.CollectionReference;
-import com.google.cloud.firestore.*;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoClient;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.ConnectionString;
-import com.mongodb.ServerAddress;
-import com.mongodb.MongoCredential;
-import com.mongodb.MongoClientOptions;
-import java.util.Arrays;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import static com.mongodb.client.model.Filters.*;
-import com.mongodb.client.model.CreateCollectionOptions;
-import com.mongodb.client.model.ValidationOptions;
 import memberDao.membroDao;
-
-import java.util.Arrays;
-import org.bson.Document;
+import Object.membro;
 
 
 @SuppressWarnings("serial")
-@WebServlet(name = "HammerServlet", value="/")
+@WebServlet(name = "HammerServlet", value="/AddMembro")
 
 public class HammerServlet  extends HttpServlet{
     membroDao membroDao;
 
 
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException   {
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException   {
         PrintWriter out = resp.getWriter();
-
-        FirestoreOptions firestoreOptions =
-                FirestoreOptions.getDefaultInstance().toBuilder()
-                        .setProjectId("api-caramuru")
-                        .build();
-        Firestore db = firestoreOptions.getService();
-        ApiFuture<QuerySnapshot> query  = db.collection("membro").orderBy("Nome").get();
+        membro membro = new membro();
+        membro.setNome(req.getParameter("nome"));
+        membro.setCPF(req.getParameter("CPF"));
+        String reqDate =  req.getParameter("data_nascimento");
+        try {
+            Date date = new SimpleDateFormat("dd/MM/yyyy").parse(reqDate);
+            membro.setDate(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        membro.setLink_foto(req.getParameter("link_foto"));
+        membro.setRegistro_UEB(req.getParameter("resgistro_ueb"));
 
         try {
-            QuerySnapshot querySnapshot = query.get();
-            List<QueryDocumentSnapshot> documents =querySnapshot.getDocuments();
-            for (QueryDocumentSnapshot document : documents){
-                out.println("Nome  "+ document.getString("Nome"));
-                out.println("REGISTRO UEB  "+ document.get("Registro_UEB"));
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            membroDao.addMembroDB(membro);
         } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
--- java/src/main/java/memberDao/membroDao.java
+++ java/src/main/java/memberDao/membroDao.java
@@ -1,8 +1,13 @@
 package memberDao;
 import Object.membro;
-import com.google.cloud.firestore.Firestore;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.*;
+
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class membroDao extends membro {
     private Firestore db;
@@ -10,15 +15,58 @@ public class membroDao extends membro {
 
     public membroDao() throws IOException {
 
-        membro = new membro();
+        FirestoreOptions firestoreOptions =
+                FirestoreOptions.getDefaultInstance().toBuilder()
+                        .setProjectId("api-caramuru")
+                        .build();
+        db = firestoreOptions.getService();
+
     }
 
     public void addMembroList(membro membro){
         membroList.add(membro);
     }
     public membro popMembroList(){
-        membroList
+       return membroList.get(0);
     }
+
+    public  String validateMembro(membro membro)throws ExecutionException , InterruptedException{
+        CollectionReference membros = db.collection("membro");
+        Query query = membros.whereEqualTo("CPF", membro.getCPF());
+        ApiFuture<QuerySnapshot> querySnapshot = query.get();
+        if(querySnapshot != null){
+            return "1";
+        }else{
+            return "0";
+        }
+    }
+
+    public  void addMembroDB(membro membro) throws ExecutionException, InterruptedException {
+
+        if(validateMembro(membro)=="0"){
+            Map<String, Object> docData = new HashMap<>();
+            docData.put("Nome", membro.getNome());
+            docData.put("CPF", membro.getCPF());
+            docData.put("Registro_UEB", membro.getRegistro_UEB());
+            docData.put("Link_foto", membro.getLink_foto());
+            docData.put("Date",membro.getDate());
+            ApiFuture<WriteResult> future = db.collection("membro").document(membro.getCPF()).set(docData);
+            System.out.println("Update time: "+ future.get().getUpdateTime());
+        }if(validateMembro(membro)=="1"){
+            System.out.println("Usuario ja cadastrado");
+        }
+
+    }
+
+    public  void updateMembroDB(membro membro)throws ExecutionException, InterruptedException{
+
+        
+
+    }
+
+
+
+
     public Firestore getDb() {
         return db;
     }
@@ -26,7 +74,5 @@ public class membroDao extends membro {
     public void setDb(Firestore db) {
         this.db = db;
     }
-
-
 
 }
